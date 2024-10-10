42ed2f19c86452e18bac1814af4739fd9e835227
Dominique Osswald <dosswald@opendrive.cl>
2024-10-09 23:55:21 -0300

    graficas 2 update

--- Memoria/graphics2.R
+++ Memoria/graphics2.R
@@ -16,18 +16,16 @@ chile_vrs <- function(hospitales_df) {
   # Cargar el mapa de Chile
   chile_map <- map_data("world", region = "Chile")
   
-  # Crear el mapa de Chile
   mapa_chile <- ggplot(data = chile) +
     geom_sf() +
-    geom_point(data = hospitales_df, aes(x = longitud, y = latitud, color = vrs, size = vrs), alpha = 0.7) +
-    scale_color_gradient(low = "red", high = "green") +  # Colores según valor
+    geom_point(data = hospitales_df, aes(x = longitud, y = latitud, color = vrs, size = (1/vrs) * 5,  text = paste("Hospital:", Nombre, "<br>VRS:", vrs, "<br>Region:", region_id)), alpha = 0.7) +
+    scale_color_gradient(low = "red", high = "green", limits = c(0.2, 1)) +  # Rango de valores para los colores
     labs(title = "Eficiencia técnica hospitales públicos Chilenos (VRS)", color = "Valor", size = "Valor") +
-    theme_minimal() +
-    labs(title = "Mapa de Chile")
+    theme_minimal()
   
   # Mostrar el mapa con ggplot2
   print(mapa_chile)
-  
+  return(mapa_chile)
   
 }
 
@@ -42,7 +40,7 @@ region_vrs <- function(hospitales_df, region) {
   
   if (region == 13){
     nombre_region = "Región Metropolitana"
-   }
+  }
   
   # Verificar si la región tiene hospitales después del filtro
   if (nrow(hospitales_df_rm) == 0) {
@@ -63,21 +61,21 @@ region_vrs <- function(hospitales_df, region) {
   # Crear el mapa de la región seleccionada
   mapa_rm <- ggplot(data = rm_comunas) +
     geom_sf(aes(geometry = geometry)) + 
-    geom_point(data = hospitales_df_rm, 
-               aes(x = longitud, y = latitud, 
-                   color = vrs, 
-                   size = vrs, 
-                   text = paste("Hospital:", Nombre, "<br>VRS:", vrs)),
-               alpha = 0.7) +
-    scale_color_gradient(low = "red", high = "green") +  # Colores según valor
+    geom_point(data = hospitales_df_rm, aes(x = longitud, y = latitud, color = vrs, size = (1/vrs) * 5,  text = paste("Hospital:", Nombre, "<br>VRS:", vrs, "<br>Region:", region_id)), alpha = 0.6)  +
+    
+    scale_color_gradient(low = "red", high = "green", limits = c(0.2, 1)) +  # Rango de valores para los colores
     labs(title = paste("Eficiencia técnica hospitales públicos en", nombre_region, "(VRS)"),
          color = "Valor", 
          size = "Valor") +
     theme_minimal()
   
-  
   return(mapa_rm)
 }
+
+
+
+
+
 
 
 
