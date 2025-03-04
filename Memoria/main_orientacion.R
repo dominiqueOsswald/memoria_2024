21383aa650a0e20b2fa9418c8d2c4d0b7fa140a6
Brayito <bryan.sepulveda@usach.cl>
2025-03-04 02:20:06 -0300

    Prueba de hipotesis

--- Memoria/main_orientacion.R
+++ Memoria/main_orientacion.R
@@ -135,17 +135,183 @@ lapply(anios, function(anio) {
 })
 
 
+
+# ---------------------------------------- #
+# ---------------------------------------- #
+# ---------------------------------------- #
 # PRUEBA DE HIPÓTESIS
 
+verificar_normalidad <- function(df, columnas) {
+  resultados <- data.frame(Columna = character(), p_valor = numeric(), Normalidad = character())
+  for (col in columnas) {
+    mat <- as.matrix(df[[col]])
+    vec <- as.numeric(mat) 
+    
+    shapiro <- shapiro.test(vec)
+    resultados <- rbind(resultados, data.frame(
+      Columna = col,
+      p_valor = shapiro$p.value,
+      Normalidad = ifelse(shapiro$p.value > 0.05, "Sí", "No")
+    ))
+  }
+  return(resultados)
+}
 
 
 
-df_2020_2021 <- resultados_usar[resultados_usar$Año %in% c(2020, 2021), "Eficiencia"]
-df_otros <- df[!(df$Año %in% c(2020, 2021)), "Eficiencia"]
+
+ef_tec <- guardar_dataframe_por_columna(resultados_usar[["oo"]], retorno)
+
+columnas <- c("2014", "2015", "2016", "2017", "2018", "2019","2020","2021","2022", "2023")
+
+normalidad_ef_tec <- verificar_normalidad(ef_tec, columnas)
+print(normalidad_todas)
+
+qqnorm(ef_tec[["2014"]])
+qqline(ef_tec[["2014"]], col = "red") 
 
 
-shapiro.test(grupo_a)  # Test de normalidad para el grupo A
-shapiro.test(grupo_b)  # Test de normalidad para el grupo B
+
+# NO SON NORMALES LOS DATOS
+
+
+# SE REALIZA PRUEBA NO PARAMETRICA:
+
+
+df_long <- ef_tec %>%
+  mutate(DMU = row_number()) %>%        # Identificador del DMU
+  pivot_longer(
+    cols = starts_with("20"),          # Ajustar si tus columnas se llaman "2014", "2015", etc.
+    names_to = "year",
+    values_to = "efficiency"
+  ) %>%
+  mutate(
+    DMU  = as.factor(DMU),
+    year = as.factor(year)  # o as.numeric si prefieres, pero factor para la prueba
+  )
+
+
+friedman.test(efficiency ~ year | DMU, data = df_long)
+# Si este test da un p-valor muy pequeño, indica que hay al menos un año diferente de los demás
+
+# revisar como difieren los años
+pairwise.wilcox.test(
+  x      = df_long$efficiency,
+  g      = df_long$year,
+  paired = TRUE,
+  p.adjust.method = "bonferroni",
+  alternative = "less"  # si tu hipótesis es que X < Y para la comparación de años
+)
+
+
+#Pairwise comparisons using Wilcoxon signed rank test with continuity correction 
+
+#data:  df_long$efficiency and df_long$year 
+
+#2014  2015  2016  2017  2018  2019  2020  2021  2022 
+#2015 0.035 -     -     -     -     -     -     -     -    
+#  2016 1.000 1.000 -     -     -     -     -     -     -    
+#  2017 1.000 1.000 1.000 -     -     -     -     -     -    
+#  2018 1.000 1.000 1.000 1.000 -     -     -     -     -    
+#  2019 1.000 1.000 1.000 1.000 1.000 -     -     -     -    
+#  2020 1.000 1.000 1.000 1.000 0.542 0.472 -     -     -    
+#  2021 1.000 1.000 1.000 1.000 0.781 0.149 1.000 -     -    
+#  2022 1.000 1.000 1.000 1.000 0.310 0.132 1.000 1.000 -    
+#  2023 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000 1.000
+
+#P value adjustment method: bonferroni 
+
+
+pairwise.wilcox.test(
+  x      = df_long$efficiency,
+  g      = df_long$year,
+  paired = TRUE,
+  p.adjust.method = "bonferroni",
+  alternative = "two.sided"  # si tu hipótesis es que X < Y para la comparación de años
+)
+
+
+#Pairwise comparisons using Wilcoxon signed rank test with continuity correction 
+
+#data:  df_long$efficiency and df_long$year 
+
+#2014    2015    2016    2017    2018    2019    2020    2021    2022   
+#2015 0.06907 -       -       -       -       -       -       -       -      
+#  2016 1.00000 0.59298 -       -       -       -       -       -       -      
+#  2017 1.00000 0.01480 0.01394 -       -       -       -       -       -      
+#  2018 0.14960 2.3e-05 1.3e-06 3.7e-05 -       -       -       -       -      
+#  2019 0.16942 2.3e-05 6.2e-06 0.00228 1.00000 -       -       -       -      
+#  2020 1.00000 0.17178 1.00000 1.00000 1.00000 0.94444 -       -       -      
+#  2021 1.00000 0.43112 1.00000 1.00000 1.00000 0.29739 1.00000 -       -      
+#  2022 1.00000 1.00000 1.00000 1.00000 0.61911 0.26358 1.00000 1.00000 -      
+#  2023 0.00033 1.8e-07 2.8e-05 0.00062 0.30616 1.00000 0.01304 0.00014 2.2e-05
+
+#P value adjustment method: bonferroni 
+
+
+
+
+
+
+
+# MALMQUIST
+
+hip_data <- malmquist_indices[["index"]]
+hip_data_2020_2021 <- hip_data[["2020_2021"]]
+
+hip_data_otros <- hip_data[,c(2,3,4,5,6,7,9,10)]
+
+columnas_previas <- c("2014_2015", "2015_2016", "2016_2017", "2017_2018", "2018_2019", "2019_2020","2020_2021","2021_2022","2022_2023")
+
+
+normalidad_todas <- verificar_normalidad(hip_data, columnas_previas)
+print(normalidad_todas)
+
+
+# ----------------------- #
+# ----------------------- #
+
+# Nombres de las columnas:
+columnas_previas <- c("2014_2015", "2015_2016", "2016_2017", "2017_2018", "2018_2019", "2019_2020","2021_2022","2022_2023")
+columna_critica <- "2020_2021"
+
+# Crear un dataframe para guardar resultados
+resultados <- data.frame(
+  Periodo_Previo = character(),
+  Estadistico_W = numeric(),
+  p_valor = numeric(),
+  stringsAsFactors = FALSE
+)
+
+# Bucle para comparar la columna crítica con cada periodo previo
+for (periodo in columnas_previas) {
+  # Aplicar Mann-Whitney (alternative = "less" para H₁: crítica < previo)
+  prueba <- wilcox.test(
+    x = hip_data[[columna_critica]], 
+    y = hip_data[[periodo]], 
+    alternative = "less",  # Evalúa si la crítica es menor que el periodo previo
+    paired = FALSE  # Muestras independientes
+  )
+  
+  # Guardar resultados
+  resultados <- rbind(resultados, data.frame(
+    Periodo_Previo = periodo,
+    Estadistico_W = prueba$statistic,
+    p_valor = prueba$p.value
+  ))
+}
+
+
+
+resultados$p_valor_ajustado <- p.adjust(resultados$p_valor, method = "bonferroni")
+
+
+print(resultados)
+
+
+
+
+
 
 
 
